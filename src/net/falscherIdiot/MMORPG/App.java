b37b1003bfb3bea4e1c13f63b6c0269276e5cd85
falscherIdiot <falscherIdiot@gmail.com>
2022-01-15 00:54:18 +0100

    moved file

---
+++ src/net/falscherIdiot/MMORPG/App.java
@@ -0,0 +1,15 @@
+package net.falscherIdiot.MMORPG;
+
+public class App {
+
+    public static final int PORT = 14122; // 14-01-2022
+
+    public static final String CONFIG_FILE_PATH = "./";
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to falscherIdiot's MMORPG");
+        System.out.println("\t\t{1} Login");
+        System.out.println("\t\t{2} Register");
+        System.out.println("\t\t{3} Quit");
+    }
+}
