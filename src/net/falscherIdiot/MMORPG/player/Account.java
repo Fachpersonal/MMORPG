ddf24b8fb53a4ed7bc28fb0e2ffbe448520ca562
falscherIdiot <falscherIdiot@gmail.com>
2022-01-15 01:43:29 +0100

    added Account

---
+++ src/net/falscherIdiot/MMORPG/player/Account.java
@@ -0,0 +1,15 @@
+package net.falscherIdiot.MMORPG.player;
+
+public class Account {
+    private String username;
+    private Stats stats;
+
+    public static void login() {
+        // ! TODO: Implement login
+    }
+
+    public static void register() {
+        // ! TODO: Implement register
+    }
+
+}
